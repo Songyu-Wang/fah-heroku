3d651806f110a045862e43ebf35825c79d16738b
Songyu-Wang <wangsongyuf@gmail.com>
2020-03-19 16:52:01 -0700

    remove useless print

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 echo "30 * * * * timeout 30s /etc/init.d/FAHClient log > /log.txt 2>&1" | crontab -
 crontab -l
-lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('
 /etc/init.d/FAHClient start
 /etc/init.d/FAHClient status
 timeout 30s /etc/init.d/FAHClient log > log.txt 2>&1
